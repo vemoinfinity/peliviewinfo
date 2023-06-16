5cba527b9150eae3d31c756520b9653b73a448cb
vemoarrozypollo <veraomar50@gmail.com>
2023-06-16 16:08:11 -0500

    arreglo pagtitle

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -22,7 +22,6 @@ const MovieDetails = () => {
   if (error) {
     return <p>Error fetching movies: {error.message}</p>;
   }
-console.log(moviesview)
   return (
     <div>
       <div className='flex'>
